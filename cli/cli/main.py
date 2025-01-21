e13ca88749c5c23c6d79e0f6f15525f3a4102136
sxsoh <soh01@allegheny.edu>
2025-01-21 15:24:15 -0500

    Update main.py

--- cli/cli/main.py
+++ cli/cli/main.py
@@ -1,6 +1,6 @@
 """a variety of numerical operations based on the value of an option to the CLI"""
 
-# TODO: add at least ten single-line comments to this file to describe individual line of code.
+o
 
 import typer
 
